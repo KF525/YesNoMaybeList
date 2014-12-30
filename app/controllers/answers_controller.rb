c435d0b2b5094d7a725da8a32fec164c77a6acf4
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-30 12:21:54 -0800

    Fix route flow

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,12 +7,12 @@ class AnswersController < ApplicationController
 
   def create
     @answer = Answer.new(params.require(:answer).permit(:name, :notes, :status))
-    user_relationship = UserRelationship.find()
+    user_relationship = UserRelationship.find_by(relationship_id: params[:relationship_id])
     @answer.user_relationship_id = user_relationship.id
     if @answer.save
-      redirect_to relationship_path
+      redirect_to relationship_path(params[:relationship_id])
     else
-      redirect_to relationship_path
+      redirect_to relationship_path(params[:relationship_id])
     end
   end
 
