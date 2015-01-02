f3b0b68b8a360617b856e2156086153d3a7cbe9e
Kate Fulton <katesuzannefulton@gmail.com>
2015-01-01 22:22:44 -0800

    Fix login js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,21 +19,21 @@
 $(function() {
     $(".login").click(function(event) {
         event.preventDefault();
-        var loginForm = '<div class="outer"><div class="inner"></div><form accept-charset="UTF-8" action="/sessions" method="post"><div style="display:none"><input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="hnR+Y3/CAJa9trXUdZWLfQw6LkKHPO39/k6+g/Z0gjc="></div><div><input id="email" name="email" type="text" oldautocomplete="remove" autocomplete="off", placeholder="email"></div><div><input name="password" placeholder="password" type="password"></div><div><input name="remember_me" type="checkbox" value="1"><label for="remember_me">Remember me</label></div><div><a class="reset-password" href="/reset_password">Forgot Password?</a></div><input name="commit" type="submit" value="Log In" class="btn"></form></div></div>'
+        var loginForm = '<form accept-charset="UTF-8" action="/sessions" method="post"><div style="display:none"><input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="hnR+Y3/CAJa9trXUdZWLfQw6LkKHPO39/k6+g/Z0gjc="></div><input id="email" name="email" type="text" oldautocomplete="remove" autocomplete="off", placeholder="email"></div><div><input name="password" placeholder="password" type="password"></div><div><input name="remember_me" type="checkbox" value="1"><label for="remember_me">Remember me</label></div><div><a class="reset-password" href="/reset_password">Forgot Password?</a></div><input name="commit" type="submit" value="Log In" class="btn"></form>'
         $(".login-info").append(loginForm);
     });
 });
 
 // USER: Update Account
-
-$(function() {
-    $(".update-email")click(function(event) {
-        event.preventDefault();
-        var updateEmail = "TEST"
-        //var updateEmail = '<form accept-charset="UTF-8" action="/sessions" method="post"><div style="display:none"><input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="hnR+Y3/CAJa9trXUdZWLfQw6LkKHPO39/k6+g/Z0gjc="></div><div><input id="email" name="email" type="text" oldautocomplete="remove" autocomplete="off", placeholder="email"></div><input name="commit" type="submit" value="Update" class="btn"></form>'
-        $(".change-email").append(updateEmail);
-    });
-});
+//
+//$(function() {
+//    $(".update-email")click(function(event) {
+//        event.preventDefault();
+//        var updateEmail = "TEST"
+//        //var updateEmail = '<form accept-charset="UTF-8" action="/sessions" method="post"><div style="display:none"><input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="hnR+Y3/CAJa9trXUdZWLfQw6LkKHPO39/k6+g/Z0gjc="></div><div><input id="email" name="email" type="text" oldautocomplete="remove" autocomplete="off", placeholder="email"></div><input name="commit" type="submit" value="Update" class="btn"></form>'
+//        $(".change-email").append(updateEmail);
+//    });
+//});
 
 
 
