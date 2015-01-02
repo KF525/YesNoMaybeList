846c15b2b5c9929a1b70aa244ac12d9fe8f15955
Kate Fulton <katesuzannefulton@gmail.com>
2015-01-01 17:13:15 -0800

    Set methods for yes no and maybe answers.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,12 +2,26 @@ class Answer < ActiveRecord::Base
   has_many :activities
   belongs_to :user_relationship
 
-  def self.relationship_answers(relationship_id) #finds all answers associated with relationship
+  #this is probably messy...
+  def self.relationship_answers(relationship_id) #finds all answers associated with a relationship/list
     user_relationships = Relationship.user_relationships_by_relationship(relationship_id)
     relationship_answers = user_relationships.collect do |user_relationship|
       Answer.where(user_relationship_id: user_relationship.id)
     end
     relationship_answers.flatten
+  end
+
+  # def self.relationship_answers(relationship_id) #finds all answers associated with relationship/list
+  # end
+
+  def self.yes_answers(relationship_id) #finds all yes answers associated with a relationship/list
+    Answer.relationship_answers(relationship_id).where(status: "yes")
+  end
+
+  def self.no_answers(relationship_id)
+  end
+
+  def self.maybe_answers(relationship_id)
   end
 
   def self.user_and_relationship_answers(relationship_id, current_user) #finds all answers made by a user for specific relationship
